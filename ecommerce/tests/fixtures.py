70cd29460401fb4ceed2a2ffd9cb502b3cde709c
waiterorg <iwaiterorg@gmail.com>
2022-03-09 14:00:34 +0330

    load fixtures for test

---
+++ ecommerce/tests/fixtures.py
@@ -0,0 +1,11 @@
+import pytest
+from django.core.management import call_command
+
+
+@pytest.fixture(scope="session")
+def db_fixture_setup(django_db_setup, django_db_blocker):
+    """
+    Load DB data fixtures
+    """
+    with django_db_blocker.unblock():
+        call_command("loaddata", "db_category_fixture.json")
