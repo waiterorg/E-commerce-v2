5920453ccfc863101712b10198d280c3113b1152
waiterorg <iwaiterorg@gmail.com>
2022-03-09 15:34:01 +0330

    create category factory and register for pytest

---
+++ ecommerce/tests/factories.py
@@ -0,0 +1,18 @@
+import factory
+import pytest
+from faker import Faker
+from pytest_factoryboy import register
+
+fake = Faker()
+from ecommerce.inventory import models
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Category
+
+    name = factory.Sequence(lambda n: "cat_slug_%d" % n)
+    slug = fake.lexify(text="cat_slug_??????")
+
+
+register(CategoryFactory)
