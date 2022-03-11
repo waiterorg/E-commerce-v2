0f67aab4a6424f9801cebfaa5f152adc68a9a4ef
waiterorg <iwaiterorg@gmail.com>
2022-03-11 16:51:39 +0330

    Add product attribute json fixture

--- ecommerce/tests/fixtures.py
+++ ecommerce/tests/fixtures.py
@@ -13,3 +13,4 @@ def db_fixture_setup(django_db_setup, django_db_blocker):
         call_command("loaddata", "db_type_fixture.json")
         call_command("loaddata", "db_brand_fixture.json")
         call_command("loaddata", "db_product_attribute_fixture.json")
+        call_command("loaddata", "db_product_attribute_value_fixture.json")
