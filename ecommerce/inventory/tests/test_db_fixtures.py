be4f23be110ac38d3a97b807fe73726c2c8e7338
waiterorg <iwaiterorg@gmail.com>
2022-03-12 14:50:59 +0330

    Create stock model test with fixture

--- ecommerce/inventory/tests/test_db_fixtures.py
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -328,3 +328,28 @@ def test_inventory_db_media_insert_data(db, media_factory):
     assert new_media.image == "images/default.png"
     assert new_media.alt_text == "a default image solid color"
     assert new_media.is_feature == 1
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, product_inventory, last_checked, units, units_sold",
+    [
+        (1, 1, "2021-09-04 22:14:18", 135, 0),
+        (8616, 8616, "2021-09-04 22:14:18", 100, 0),
+    ],
+)
+def test_inventory_db_stock_dataset(
+    db,
+    db_fixture_setup,
+    id,
+    product_inventory,
+    last_checked,
+    units,
+    units_sold,
+):
+    result = models.Stock.objects.get(id=id)
+    result_last_checked = result.last_checked.strftime("%Y-%m-%d %H:%M:%S")
+    assert result.product_inventory.id == product_inventory
+    assert result_last_checked == last_checked
+    assert result.units == units
+    assert result.units_sold == units_sold
