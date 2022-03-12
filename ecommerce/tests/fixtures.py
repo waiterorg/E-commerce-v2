776dd0a24a97853fb438d919b5e9d1d2d9e49f51
waiterorg <iwaiterorg@gmail.com>
2022-03-12 14:51:57 +0330

    Add stock fixture to fixture setup

--- ecommerce/tests/fixtures.py
+++ ecommerce/tests/fixtures.py
@@ -16,3 +16,4 @@ def db_fixture_setup(django_db_setup, django_db_blocker):
         call_command("loaddata", "db_product_attribute_value_fixture.json")
         call_command("loaddata", "db_product_inventory_fixture.json")
         call_command("loaddata", "db_media_fixture.json")
+        call_command("loaddata", "db_stock_fixture.json")
