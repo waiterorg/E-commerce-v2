3d7bd600cf7dba1cc2ce2494d8a1295cf3ef2e48
waiterorg <iwaiterorg@gmail.com>
2022-03-28 01:43:17 +0430

    Add rest frame work settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,6 +138,11 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
+}
+
 ELASTICSEARCH_DSL = {
     "default": {"hosts": "localhost:9200"},
 }
