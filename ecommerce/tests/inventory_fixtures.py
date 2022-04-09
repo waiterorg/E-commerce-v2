5a3f249a099c35709d96bba3abd728b99e038916
waiterorg <iwaiterorg@gmail.com>
2022-04-10 00:36:56 +0430

    Add category fixture for test view

---
+++ ecommerce/tests/inventory_fixtures.py
@@ -0,0 +1,31 @@
+import pytest
+from ecommerce.inventory.models import Category
+
+
+@pytest.fixture
+def category_with_multiple_children(db):
+    record = Category.objects.build_tree_nodes(
+        {
+            "id": 1,
+            "name": "parent",
+            "slug": "parent",
+            "children": [
+                {
+                    "id": 2,
+                    "parent_id": 1,
+                    "name": "child",
+                    "slug": "child",
+                    "children": [
+                        {
+                            "id": 3,
+                            "parent_id": 2,
+                            "name": "grandchild",
+                            "slug": "grandchild",
+                        }
+                    ],
+                }
+            ],
+        }
+    )
+    category = Category.objects.bulk_create(record)
+    return category
