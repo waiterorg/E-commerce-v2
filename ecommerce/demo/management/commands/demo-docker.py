7885d7178d6c6862f68fb83c10be5fe658340bcf
waiterorg <iwaiterorg@gmail.com>
2022-04-09 15:09:34 +0430

    create docker compose up command

---
+++ ecommerce/demo/management/commands/demo-docker.py
@@ -0,0 +1,7 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self):
+        call_command("docker-compose up -d")
