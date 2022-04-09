3baf3c9f21baae3df5b43b464d9cf40a66e42859
waiterorg <iwaiterorg@gmail.com>
2022-04-10 00:39:08 +0430

    Create category list view - get all categories

--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from ecommerce.api.serializer import CategorySerializer
+from ecommerce.inventory.models import Category
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class CategoryList(APIView):
+    """
+    Return list of all categories
+    """
+
+    def get(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
