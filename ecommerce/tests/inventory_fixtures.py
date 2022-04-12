c0bbbc945e05908881df4f3a5d254e75731f2089
waiterorg <iwaiterorg@gmail.com>
2022-04-13 00:51:39 +0430

    Create product inventory fixture

--- ecommerce/tests/inventory_fixtures.py
+++ ecommerce/tests/inventory_fixtures.py
@@ -1,5 +1,14 @@
 import pytest
-from ecommerce.inventory.models import Category, Product
+from ecommerce.inventory.models import (
+    Brand,
+    Category,
+    Media,
+    Product,
+    ProductAttribute,
+    ProductAttributeValue,
+    ProductInventory,
+    ProductType,
+)
 
 
 @pytest.fixture
@@ -49,3 +58,74 @@ def single_product(db, child_category):
         is_active=True,
     )
     return product
+
+
+@pytest.fixture
+def product_attribute(db):
+    product_attribute = ProductAttribute.objects.create(
+        name="default", description="default"
+    )
+    return product_attribute
+
+
+@pytest.fixture
+def product_type(db, product_attribute):
+    product_type = ProductType.objects.create(name="default")
+    product_attribute = product_attribute
+
+    product_type.product_type_attributes.add(product_attribute)
+
+    return product_type
+
+
+@pytest.fixture
+def brand(db):
+    brand = Brand.objects.create(name="default")
+    return brand
+
+
+@pytest.fixture
+def product_attribute_value(db, product_attribute):
+    product_attribute_value = ProductAttributeValue.objects.create(
+        product_attribute=product_attribute,
+        attribute_value="default",
+    )
+    return product_attribute_value
+
+
+@pytest.fixture
+def single_sub_product_with_media_and_attributes(
+    db, single_product, product_type, brand, product_attribute_value
+):
+
+    sub_product = ProductInventory.objects.create(
+        sku="123456789",
+        upc="100000000001",
+        product_type=product_type,
+        product=single_product,
+        brand=brand,
+        is_active=True,
+        is_default=True,
+        retail_price="199.99",
+        store_price="99.99",
+        sale_price="9.99",
+        is_on_sale=False,
+        is_digital=False,
+        weight=1000.0,
+    )
+
+    media = Media.objects.create(
+        product_inventory=sub_product,
+        image="images/default.png",
+        alt_text="default",
+        is_feature=True,
+    )
+
+    product_attribute_value = product_attribute_value
+    sub_product.attribute_values.add(product_attribute_value)
+
+    return {
+        "inventory": sub_product,
+        "media": media,
+        "attribute": product_attribute_value,
+    }
