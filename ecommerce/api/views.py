074e8df4be700c5f55cd0571c1566a0922ac9d26
waiterorg <iwaiterorg@gmail.com>
2022-04-13 00:52:57 +0430

    Create get product inventory by web id view

--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from ecommerce.api.serializer import CategorySerializer, ProductSerializer
-from ecommerce.inventory.models import Category, Product
+from ecommerce.api.serializer import (
+    CategorySerializer,
+    ProductInventorySerializer,
+    ProductSerializer,
+)
+from ecommerce.inventory.models import Category, Product, ProductInventory
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -24,4 +28,15 @@ class ProductByCategory(APIView):
     def get(self, request, query=None):
         queryset = Product.objects.filter(category__slug=query)
         serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class ProductInventoryByWebId(APIView):
+    """
+    Return Sub Product by WebId
+    """
+
+    def get(self, request, query=None):
+        queryset = ProductInventory.objects.filter(product__web_id=query)
+        serializer = ProductInventorySerializer(queryset, many=True)
         return Response(serializer.data)
