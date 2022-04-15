db3b0140bc0183196510dea79beb168cbe89391e
waiterorg <iwaiterorg@gmail.com>
2022-04-16 00:28:36 +0430

    Create schemas inventory - brand - media - product type - product attribute

--- ecommerce/dninja/schema.py
+++ ecommerce/dninja/schema.py
@@ -1,5 +1,15 @@
-from ecommerce.inventory.models import Category, Product
-from ninja import ModelSchema
+from typing import List
+
+from ecommerce.inventory.models import (
+    Brand,
+    Category,
+    Media,
+    Product,
+    ProductAttributeValue,
+    ProductInventory,
+    ProductType,
+)
+from ninja import Field, ModelSchema
 
 
 class CategorySchema(ModelSchema):
@@ -14,4 +24,53 @@ class ProductSchema(ModelSchema):
         model_fields = [
             "name",
             "web_id",
+        ]
+
+
+class BrandSchema(ModelSchema):
+    class Config:
+        model = Brand
+        model_fields = ["name"]
+
+
+class MediaSchema(ModelSchema):
+
+    image: str
+
+    class Config:
+        model = Media
+        model_fields = ["image", "alt_text"]
+
+
+class ProductTypeSchema(ModelSchema):
+    class Config:
+        model = ProductType
+        model_fields = ["name"]
+
+
+class ProductAttributeValueSchema(ModelSchema):
+    class Config:
+        model = ProductAttributeValue
+        model_fields = "__all__"
+
+
+class InventorySchema(ModelSchema):
+
+    brand: BrandSchema
+    product: ProductSchema
+    media: List[MediaSchema] = Field(alias="media")
+    product_type: ProductTypeSchema
+    attributes: List[ProductAttributeValueSchema] = Field(
+        alias="attribute_values"
+    )
+
+    class Config:
+        model = ProductInventory
+        model_fields = [
+            "id",
+            "sku",
+            "store_price",
+            "is_default",
+            "is_on_sale",
+            "weight",
         ]
