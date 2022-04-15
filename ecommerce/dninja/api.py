ea317e4ce2b47ab0b8c86952f2e4fb2fc6c682e7
waiterorg <iwaiterorg@gmail.com>
2022-04-16 00:22:12 +0430

    Add get product inventory by product web id

--- ecommerce/dninja/api.py
+++ ecommerce/dninja/api.py
@@ -20,3 +20,9 @@ def category_list(request):
 def product_list(request, category_slug: str):
     qs = Product.objects.filter(category__slug=category_slug)
     return qs
+
+
+@api.get("/inventory/{web_id}/", response=List[InventorySchema])
+def inventory_list(request, web_id: str):
+    qs = ProductInventory.objects.filter(product__web_id=web_id)
+    return qs
