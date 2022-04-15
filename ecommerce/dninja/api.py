fcb1f23c6af2b46bcafffe4ebf554a63686f27f3
waiterorg <iwaiterorg@gmail.com>
2022-04-16 00:13:43 +0430

    Add get product by category slug api

--- ecommerce/dninja/api.py
+++ ecommerce/dninja/api.py
@@ -1,8 +1,8 @@
 from typing import List
 
-from ecommerce.inventory.models import Category
+from ecommerce.inventory.models import Category, Product
 from ninja import NinjaAPI
-from .schema import CategorySchema
+from .schema import CategorySchema, ProductSchema
 
 api = NinjaAPI()
 
@@ -10,4 +10,13 @@ api = NinjaAPI()
 @api.get("/inventory/category/", response=List[CategorySchema])
 def category_list(request):
     qs = Category.objects.all()
+    return qs
+
+
+@api.get(
+    "/inventory/products/category/{category_slug}/",
+    response=List[ProductSchema],
+)
+def product_list(request, category_slug: str):
+    qs = Product.objects.filter(category__slug=category_slug)
     return qs
