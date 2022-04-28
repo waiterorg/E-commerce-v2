d19cbbb1b25ea637f6ec76ce4f1655db04e4f2e6
waiterorg <iwaiterorg@gmail.com>
2022-04-28 18:59:01 +0430

    Add promotion form in admin page

--- ecommerce/promotion/admin.py
+++ ecommerce/promotion/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+from .tasks import promotion_prices
+
+
+class ProductOnPromotion(admin.StackedInline):
+    model = models.Promotion.products_on_promotion.through
+    extra = 4
+    raw_id_fields = ("product_inventory_id",)
+
+
+class PromotionList(admin.ModelAdmin):
+    model = models.Promotion
+    inlines = (ProductOnPromotion,)
+    list_display = ("name", "is_active", "promo_start", "promo_end")
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        promotion_prices.delay(obj.promo_reduction, obj.id)
+
+
+admin.site.register(models.Promotion, PromotionList)
