bc032ba7c705c62bdb8b2c7e34f464090e40a6a7
waiterorg <iwaiterorg@gmail.com>
2022-04-29 18:13:24 +0430

    Create test for promotion is active management task

--- ecommerce/promotion/tests/test_promotion_management.py
+++ ecommerce/promotion/tests/test_promotion_management.py
@@ -20,3 +20,24 @@ def test_promotion_price_reduction(
         promotion_id=promotion_multi_variant.id
     )
     assert new_price.promo_price == result
+
+
+@pytest.mark.parametrize(
+    "start, end, expected",
+    [
+        (date.today(), date.today() + timedelta(5), True),
+        (date.today() - timedelta(10), date.today() + timedelta(5), True),
+        (date.today() - timedelta(10), date.today() - timedelta(5), False),
+        (date.today() + timedelta(5), date.today() + timedelta(10), False),
+    ],
+)
+def test_promotion_management(
+    start, end, expected, celery_app, celery_worker, promotion_multi_variant
+):
+
+    promotion_multi_variant.promo_start = start
+    promotion_multi_variant.promo_end = end
+    promotion_multi_variant.save(update_fields=["promo_start", "promo_end"])
+    promotion_management()
+    promotion = Promotion.objects.all().first()
+    assert promotion.is_active == expected
