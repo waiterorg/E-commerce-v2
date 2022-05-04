2c622d2e85b8d9ae11533062a287b1d2b58cf319
waiterorg <iwaiterorg@gmail.com>
2022-05-04 17:50:02 +0430

    config swagger in urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from ecommerce.api.views import (
     CategoryList,
@@ -23,7 +26,25 @@ from ecommerce.api.views import (
 )
 from ecommerce.search.views import SearchProductInventory
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Ecommerce-v2 API",
+        default_version="v1",
+        description="Products API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="iwaiterorg@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/search/<str:query>/", SearchProductInventory.as_view()),
